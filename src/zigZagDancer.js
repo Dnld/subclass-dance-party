a746ae4b30735dfada089dc890c411026f830068
Donald Steinert <steinert.donald@gmail.com>
2015-08-24 22:19:10 -0400

    update zig zag implementation, adds tests for zig zag dancer

--- src/zigZagDancer.js
+++ src/zigZagDancer.js
@@ -13,12 +13,13 @@ ZigZagDancer.prototype.step = function() {
   // hide the existing node
   this.$node.hide();
   // calculate new top and left
+
+  var randomRange = function(minNum, maxNum){
+    return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+  };
   
-  var range = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6,
-    7, 8, 9, 10];
-  
-  this.top = this.top + range[Math.floor(Math.random() * 21)];
-  this.left = this.left + range[Math.floor(Math.random() * 21)];
+  this.top = this.top + randomRange(-10, 10);
+  this.left = this.left + randomRange(-10, 10);
   // set position with top and left
   
   this.setPosition(this.top, this.left);
